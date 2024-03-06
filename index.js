ebd3a35ddcb8509f36402135388063e3892b43ff
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-07 02:44:41 +0530

    pkg code commited

---
+++ index.js
@@ -0,0 +1,49 @@
+const xml2js = require('xml2js');
+const fs = require('fs');
+const xmldataconverter = (xml) => {
+    const parser = new xml2js.Parser({explicitArray: false, mergeAttrs: true});
+    parser.parseString(xml, (err, result) => {
+        if (err) {
+            throw 'Input Proper XML Data';
+        }
+        const json = JSON.stringify(result, null, 4);
+        console.log(json);
+    });
+};
+function csvdataconverter(csv) {
+    const lines = csv.split('\n');
+    // console.log(lines);
+    const headers = lines[0].split(',');
+    // console.log(headers);
+    // console.log(lines.slice(1));
+    return lines.slice(1).map(line => {
+        const set = line.split(',');
+        // console.log(set);
+        return headers.reduce((obj, header, i) => {
+
+
+            console.log(typeof(header));
+            // header = JSON.stringify(header);
+            obj[header] = set[i]; 
+            return obj;
+        },{});
+    });
+}
+const identity = (data) =>{
+
+    const len = data.length ;
+
+    const variable = data.indexOf("<")
+    console.log(variable);
+    if(variable==0){
+        return xmldataconverter(data)
+    }
+
+    return console.log(csvdataconverter(data)); 
+
+
+}
+module.exports = identity ;
+
+
+
